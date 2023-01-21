a3e56fa2979f8505a2eb69e7b34a51e3694da1c4
lyndonk9 <90118166+lyndonk9@users.noreply.github.com>
2023-01-21 14:40:10 -0500

    Music + Ambience

--- Assets/Scripts/Audio_Player_Script.cs
+++ Assets/Scripts/Audio_Player_Script.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class Audio_Player_Script : MonoBehaviour
 {
+
+    public StudioEventEmitter Ambience;
+    public StudioEventEmitter Music;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        Ambience = GetComponent<StudioEventEmitter>();
+        Ambience.Play();
+
+        Music = GetComponent<StudioEventEmitter>();
+        Music.Play();
+
     }
 
     // Update is called once per frame
@@ -16,3 +27,4 @@ public class Audio_Player_Script : MonoBehaviour
         
     }
 }
+
