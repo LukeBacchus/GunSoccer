cdbab9defa699836e5047b34e4559f5d73affdf5
Nina W <ny.wang@mail.utoronto.ca>
2023-01-21 15:02:22 -0500

    Added crosshair and scoreboard

--- Assets/Scripts/GoalBehavior.cs
+++ Assets/Scripts/GoalBehavior.cs
@@ -1,17 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoalBehavior : MonoBehaviour
 {
+    private TMPro.TextMeshProUGUI teamOneScoreText;
+    private TMPro.TextMeshProUGUI teamTwoScoreText;
+
     public int team;
+    private Scores scores;
+
     // Start is called before the first frame update
+    private void Start()
+    {
+        scores = GameObject.Find("GameManager").GetComponent<Scores>();
+        teamOneScoreText = GameObject.Find("Team 1 Score").GetComponent<TMPro.TextMeshProUGUI>();
+        teamTwoScoreText = GameObject.Find("Team 2 Score").GetComponent<TMPro.TextMeshProUGUI>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Soccer")
         {
-            Debug.Log("Team " + team + " got scored on.");
             collision.gameObject.transform.position = new Vector3(0, 5, 0);
+
+            if (team == 1)
+            {
+                scores.teamTwoScore += 1;
+                teamTwoScoreText.text = "Team 2 Score: " + scores.teamTwoScore;
+            } else
+            {
+                scores.teamOneScore += 1;
+                teamOneScoreText.text = "Team 1 Score: " + scores.teamOneScore;
+            }
         }
     }
 }
---
+++ Assets/Scripts/Scores.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scores : MonoBehaviour
+{
+
+    public int teamOneScore;
+    public int teamTwoScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        teamOneScore = 0;
+        teamTwoScore = 0;
+    }
+}
