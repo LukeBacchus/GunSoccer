090e669b3a1564162ef155ff9ece3d63d38248d7
calebxcaleb <calebxcaleb@gmail.com>
2023-02-16 15:23:19 -0500

    Player reset

--- Assets/Scripts/GoalBehavior.cs
+++ Assets/Scripts/GoalBehavior.cs
@@ -8,6 +8,10 @@ public class GoalBehavior : MonoBehaviour
     private TMPro.TextMeshProUGUI teamOneScoreText;
     private TMPro.TextMeshProUGUI teamTwoScoreText;
 
+    [SerializeField] private GameObject[] players;
+    private Vector3[] playerPoss = new Vector3[4];
+    private Quaternion[] playerRots = new Quaternion[4];
+
     public int team;
     private GameStats gameStats;
 
@@ -17,6 +21,13 @@ public class GoalBehavior : MonoBehaviour
         gameStats = GameObject.Find("GameManager").GetComponent<GameStats>();
         teamOneScoreText = GameObject.Find("Team 1 Score").GetComponent<TMPro.TextMeshProUGUI>();
         teamTwoScoreText = GameObject.Find("Team 2 Score").GetComponent<TMPro.TextMeshProUGUI>();
+
+        for(int i = 0; i < players.Length; i++){
+            playerPoss[i] = players[i].gameObject.transform.position;
+            playerRots[i] = players[i].gameObject.transform.rotation;
+        }
+
+        Debug.Log("Pos init: " + playerPoss[0]);
     }
     
     private void OnCollisionEnter(Collision collision)
@@ -25,6 +36,13 @@ public class GoalBehavior : MonoBehaviour
         {
             collision.gameObject.transform.position = new Vector3(0, 5, 0);
             collision.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+
+            Debug.Log("Pos: " + playerPoss[0]);
+
+            for(int i = 0; i < players.Length; i++){
+                players[i].gameObject.transform.position = playerPoss[i];
+                players[i].gameObject.transform.rotation = playerRots[i];
+            }
 
             if (team == 1)
             {
