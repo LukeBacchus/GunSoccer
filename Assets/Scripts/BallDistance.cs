1367832846e0caed2caba2d9fddaf3440e45dcbc
lyndonk9 <90118166+lyndonk9@users.noreply.github.com>
2023-03-23 17:24:08 -0400

    Audience SFX

--- Assets/Scripts/BallDistance.cs
+++ Assets/Scripts/BallDistance.cs
@@ -5,41 +5,7 @@ using FMODUnity;
 
 public class BallDistance : MonoBehaviour
 {
-    [SerializeField] private GameObject goal;
-    [SerializeField] private GameObject ball;
-    public FMODUnity.EventReference cheer;
-
-    private FMOD.Studio.EventInstance instance;
-    private float dist_to_goal;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-       instance = FMODUnity.RuntimeManager.CreateInstance(cheer);
-       instance.setParameterByName("Intensity", 0);
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        dist_to_goal = Vector3.Distance(goal.transform.position, ball.transform.position);
-        Debug.Log("distance is " + dist_to_goal);
-
-        if (dist_to_goal < 50)
-        {
-            
-            instance.setParameterByName("Intensity", 100);
-            RuntimeManager.PlayOneShot("event:/cheer");
-
-            if (dist_to_goal > 50)
-            {
-                instance.setParameterByName("Intensity", 0);
-            }
-
     
-
-        } 
-    }
+        
+    
 }
