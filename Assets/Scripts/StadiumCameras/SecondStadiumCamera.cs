a36c2160bd5b68c5d40bd5f08b00c5bb23c8baa3
Nina W <ny.wang@mail.utoronto.ca>
2023-03-26 19:40:39 -0400

    Modify stadium camera for new stadium. Block players from entering goal

--- Assets/Scripts/StadiumCameras/SecondStadiumCamera.cs
+++ Assets/Scripts/StadiumCameras/SecondStadiumCamera.cs
@@ -3,10 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StadiumCamera : MonoBehaviour
+public class SecondStadiumCamera : StadiumCamera
 {
     [SerializeField]
     private GameObject stadium;
+    [SerializeField]
+    private GameObject mapViewTarget;
     [SerializeField]
     private GameObject rotateTarget;
     [SerializeField]
@@ -20,15 +22,38 @@ public class StadiumCamera : MonoBehaviour
 
     private WaitForSeconds fadeWait = new WaitForSeconds(1);
 
-    public IEnumerator IntroPan(Action action)
+    public override IEnumerator IntroPan(Action action)
     {
-        // Circle view stadium
-        transform.localPosition = new Vector3(0, 4, -6);
-        transform.localEulerAngles = new Vector3(35, 0, 0);
-        transform.SetParent(rotateTarget.transform);
+        // View map
+        transform.SetParent(mapViewTarget.transform);
+        transform.localPosition = Vector3.zero;
+        transform.localEulerAngles = new Vector3(-25, 0, 0);
 
-        float count = 4.5f;
+        float count = 5.5f;
         bool fadein = false;
+        while (count > 0)
+        {
+            count -= Time.deltaTime;
+            transform.Translate((mapViewTarget.transform.right * 110 - mapViewTarget.transform.forward * 30) * Time.deltaTime, Space.World);
+
+            if (count <= 1 && !fadein)
+            {
+                blackScreenAnimator.SetTrigger("fade_in");
+                fadein = true;
+            }
+            yield return null;
+        }
+
+
+        // Circle view stadium
+        transform.SetParent(stadium.transform);
+        transform.localPosition = new Vector3(0, 20, -30);
+        transform.localEulerAngles = new Vector3(40, 0, 0);
+        transform.SetParent(rotateTarget.transform);
+        blackScreenAnimator.SetTrigger("fade_out");
+
+        count = 4f;
+        fadein = false;
         while (count > 0)
         {
             count -= Time.deltaTime;
@@ -48,12 +73,12 @@ public class StadiumCamera : MonoBehaviour
         transform.localEulerAngles = Vector3.zero;
         blackScreenAnimator.SetTrigger("fade_out");
 
-        count = 3.5f;
+        count = 4f;
         fadein = false;
         while (count > 0)
         {
             count -= Time.deltaTime;
-            transform.Translate((redTeamTarget.transform.right * 13 + redTeamTarget.transform.forward * 4.5f) * Time.deltaTime);
+            transform.Translate((redTeamTarget.transform.right * 13 + redTeamTarget.transform.forward * 3.5f) * Time.deltaTime, Space.World);
 
             if (count <= 1 && !fadein)
             {
@@ -69,12 +94,12 @@ public class StadiumCamera : MonoBehaviour
         transform.localEulerAngles = Vector3.zero;
         blackScreenAnimator.SetTrigger("fade_out");
 
-        count = 3.5f;
+        count = 4f;
         fadein = false;
         while (count > 0)
         {
             count -= Time.deltaTime;
-            transform.Translate((-blueTeamTarget.transform.right * 13 + -blueTeamTarget.transform.forward * 4.5f) * Time.deltaTime);
+            transform.Translate((blueTeamTarget.transform.right * 13 + blueTeamTarget.transform.forward * 3.5f) * Time.deltaTime, Space.World);
 
             if (count <= 1 && !fadein)
             {
