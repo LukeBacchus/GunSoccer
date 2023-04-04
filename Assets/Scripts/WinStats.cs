8baf6fe1fe59cd7968d166efa59c77b9a302fbce
Nina W <ny.wang@mail.utoronto.ca>
2023-04-04 14:40:21 -0400

    Add functionality to game over

--- Assets/Scripts/WinStats.cs
+++ Assets/Scripts/WinStats.cs
@@ -6,10 +6,7 @@ using UnityEngine.UI;
 
 public class WinStats: MonoBehaviour 
 {
-    private TMPro.TextMeshProUGUI winnerText;
     public float timer;
-    private GameStats gameStats;
-    private string winner;
     [SerializeField]
     private Button replayButton;
     [SerializeField]
@@ -27,22 +24,6 @@ public class WinStats: MonoBehaviour
         List<List<GameObject>> winButtons = new List<List<GameObject>> { new List<GameObject> { replayButton.gameObject }, 
             new List<GameObject> { quitButton.gameObject }, new List<GameObject> { creditsButton.gameObject } };
         winSelector = new MenuSelectionHelper(winButtons, 0, 2, new List<int> { 1, 2, 3, 4 });
-
-        // gameStats = GameObject.Find("GameManager").GetComponent<GameStats>();
-        // if (gameStats.teamOneScore > gameStats.teamTwoScore) {
-        //     winner = "Team 1";
-        // }
-        // else if (gameStats.teamTwoScore > gameStats.teamOneScore) {
-        //     winner = "Team 2";
-        // }
-        // else {
-        //     winner = "No One";
-        // }
-        // winnerText = GameObject.Find("Win Text").GetComponent<TMPro.TextMeshProUGUI>();
-        // timer = 15.99f;
-        // winnerText.text = winner + " Won!!! Congrats!!! Returning To Main Menu In: 15";
-
-        // StartCoroutine(UpdateWinScreen());
     }
     public void UpdateWinScreen()
     {
@@ -51,31 +32,21 @@ public class WinStats: MonoBehaviour
         {
             winSelector.InvokeSelection();
         }
-        // while (timer > 0)
-        // {
-        //     timer -= Time.deltaTime;
-        //     winnerText.text = winner + " Won!!! Congrats!!! Returning To Main Menu In: " + (int) timer;
-        //     yield return null;
-        // }
-
-        // SceneManager.LoadScene("Start Screen"); 
     }
 
     private void SelectedReplay()
     {
-        Debug.Log("Selected replay. Replay Not Implemented yet.");
+        SceneManager.LoadScene("Start Screen");
     }
 
     private void SelectedQuit()
     {
-        SceneManager.LoadScene("Start Screen"); 
+        Application.Quit();
     }
 
     private void SelectedCredits()
     {
         Debug.Log("Selected credits. Credits Not Implemented yet.");
     }
-
-
 
 }
